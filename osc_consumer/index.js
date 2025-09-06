0709ec1bba96fc50c007df24769ed85e6a78b314
Joe Cheng <joe.cheng2001@gmail.com>
2025-09-06 14:27:59 +0100

    added audiotool sdk

--- osc_consumer/index.js
+++ osc_consumer/index.js
@@ -1,10 +1,25 @@
-const OSC = require("osc-js");
+import * as OSC from "osc-js";
+import { createAudiotoolClient } from "@audiotool/nexus"
 
-const osc = new OSC({plugin: new OSC.DatagramPlugin()});
+const osc = new OSC.default({plugin: new OSC.default.DatagramPlugin()});
+const client = await createAudiotoolClient({pat: process.env.API_KEY });
+
+const nexus = await client.createSyncedDocument({
+  mode: "online",
+  project: process.env.PROJECT_URL,
+});
+
+await nexus.start();
+
+nexus.modify((t) => {
+  const mixer = t.entities.ofTypes("mixerOut").getOne()
+  if (mixer != undefined) {
+    t.update(mixer.fields.postGain, 0.5)
+  }
+})
 
 osc.on('*', message => {
   console.log(message.args);
 });
 
 osc.open({ port: 1130 });
-console.log(osc);
